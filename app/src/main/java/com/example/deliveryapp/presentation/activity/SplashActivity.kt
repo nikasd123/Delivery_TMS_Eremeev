64ae24fbfd21e8855f13353e11fc29886949d53f
nikita <nikita_eremeev.95@mail.ru>
2023-06-06 21:22:54 +0300

    SplashActivity

---
+++ app/src/main/java/com/example/deliveryapp/presentation/activity/SplashActivity.kt
@@ -0,0 +1,27 @@
+package com.example.deliveryapp.presentation.activity
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.example.deliveryapp.R
+
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+
+    private val SPLASH_TIME_OUT = 3000L
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, SPLASH_TIME_OUT)
+    }
+}
