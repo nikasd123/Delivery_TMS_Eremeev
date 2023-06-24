a60e2efbc30f9eb5484fff6f91ed4536fbd2efc6
nikita <nikita_eremeev.95@mail.ru>
2023-06-24 23:57:34 +0300

    base UI

--- app/src/main/java/com/example/deliveryapp/presentation/fragments/ShoppingCartFragment.kt
+++ app/src/main/java/com/example/deliveryapp/presentation/fragments/ShoppingCartFragment.kt
@@ -6,18 +6,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.deliveryapp.R
+import com.example.deliveryapp.databinding.FragmentDishesBinding
 import com.example.deliveryapp.databinding.FragmentShoppingCartBinding
 import com.example.deliveryapp.presentation.viewmodels.ShoppingCartViewModel
 
-class ShoppingCartFragment : Fragment() {
+class ShoppingCartFragment : Fragment(R.layout.fragment_shopping_cart) {
     private lateinit var viewModel: ShoppingCartViewModel
     private lateinit var binding: FragmentShoppingCartBinding
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_shopping_cart, container, false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentShoppingCartBinding.bind(view)
+
+        val layoutManager = LinearLayoutManager(requireContext())
+        binding.recyclerView.layoutManager = layoutManager
     }
 }
