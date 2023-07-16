1132ca78c7a15e7a0128c42ed9356bda6ba74179
nikit <nikita_eremeev.95@mail.ru>
2023-07-17 02:22:12 +0300

    retrofit

--- app/src/main/java/com/example/deliveryapp/data/remote/api/PixelsApi.kt
+++ app/src/main/java/com/example/deliveryapp/data/remote/api/PixelsApi.kt
@@ -1,17 +1,20 @@
 package com.example.deliveryapp.data.remote.api
 
+import com.example.deliveryapp.BuildConfig
 import com.example.deliveryapp.data.remote.dto.ResponseDto
+import com.google.gson.internal.GsonBuildConfig
 import retrofit2.http.GET
 import retrofit2.http.Headers
 import retrofit2.http.Query
+import java.util.Properties
 
 
 interface PixelsApi {
-    private companion object {
+    companion object {
         const val GET_IMAGES_REQUEST = "search"
         const val CATEGORY_PARAM = "query"
         const val API_KEY_PARAM = "Authorization"
-        const val API_KEY = "Wy6pUdHfjaqywKlfBXiFOogU5gFYjMjwPRnqi8y9Ko3uaFJRB22J50lL"
+        const val API_KEY = BuildConfig.API_KEY
     }
 
     @Headers("$API_KEY_PARAM: $API_KEY")
