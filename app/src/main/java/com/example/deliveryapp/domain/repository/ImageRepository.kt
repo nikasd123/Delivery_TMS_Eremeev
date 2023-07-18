f0adf394c6f7719ad535b1ba0baaebe86e845b30
nikit <nikita_eremeev.95@mail.ru>
2023-07-18 17:26:27 +0300

    add refactor. Rename interface function

--- app/src/main/java/com/example/deliveryapp/domain/repository/ImageRepository.kt
+++ app/src/main/java/com/example/deliveryapp/domain/repository/ImageRepository.kt
@@ -3,5 +3,5 @@ package com.example.deliveryapp.domain.repository
 import com.example.deliveryapp.data.remote.dto.ImageDto
 
 interface ImageRepository {
-    suspend fun getImages(category: String): List<ImageDto>
+    suspend fun getImage(category: String): List<ImageDto>
 }
