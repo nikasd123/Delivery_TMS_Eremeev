e15715187008a599ec6e35287017ee1b423bf5a8
nikit <nikita_eremeev.95@mail.ru>
2023-07-22 01:27:15 +0300

    naming refactor

--- app/src/main/java/com/example/deliveryapp/data/local/AppDatabase.kt
+++ app/src/main/java/com/example/deliveryapp/data/local/AppDatabase.kt
@@ -4,6 +4,7 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 
 @Database(entities = [DishEntity::class], version = 1)
-abstract class DishesDatabase : RoomDatabase(){
+abstract class AppDatabase : RoomDatabase(){
     abstract fun dishesDao(): DishesDao
+    abstract fun userDao(): UserDao
 }
